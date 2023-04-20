74e622d0bb7038152a3ecb10646d6d06cbcb061e
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-04-20 16:09:38 +0100

    Exercise 6
    PickupPowerUP() and TakeDamage() test

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace GameSix
 {
     class Program
@@ -9,10 +8,8 @@ namespace GameSix
             //Ask user for the number of foes
             Console.Write("How many foes? ");
             int FoesAvailable = int.Parse(Console.ReadLine());
-
             //Sort foes in an array
             Foe[] foes = new Foe[FoesAvailable];
-
             //Ask the user for each foe's name
             for (int i = 0; i < FoesAvailable; i++)
             {
@@ -20,11 +17,24 @@ namespace GameSix
                 string name = Console.ReadLine();
                 foes[i] = new Foe(name);
             }
-
             //Print all foes' names one by one
             for (int i = 0; i < FoesAvailable; i++)
             {
                 Console.WriteLine("Foe " + (i + 1) + ": " + foes[i].GetName());
+            }
+
+             //PickupPowerUp test
+            foes[0].PickupPowerUp(PowerUp.Health, 34.12f);
+            foes[0].PickupPowerUp(PowerUp.Shield, 61.36f);
+
+            //TakeDamage test
+            foes[0].TakeDamage(44.47f);
+
+            //Print all foes' health and shield
+            for (int i = 0; i < FoesAvailable; i++)
+            {
+                Console.WriteLine(" Foe " + (i + 1) + ": " + foes[i].GetName() + " has " +
+                foes[i].GetHealth() + " health and " + foes[i].GetShield() + " shield.");
             }
         }
     }
