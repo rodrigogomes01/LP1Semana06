4d1374a626a9327ef550aa64b2d61829419b9e51
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-04-20 15:50:46 +0100

    Exercise 3
    Added 2 new methods to class Foe

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -15,5 +15,21 @@ namespace GameSix
             health = 100;
             shield = 0;
         }
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
     }
 }
