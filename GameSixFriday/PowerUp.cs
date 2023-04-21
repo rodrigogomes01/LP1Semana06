d6a722c0ae9105f2f743b9cbdedaaa1631435b5a
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-04-21 14:09:56 +0100

    Exercise 9
    Subtituted class Program with a new class

--- GameSixFriday/PowerUp.cs
+++ GameSixFriday/PowerUp.cs
@@ -1,8 +1,35 @@
+﻿using System;
+
 namespace GameSixFriday
 {
-    public enum PowerUp
+    public class Program
     {
-        Health,
-        Shield,
+        private static void Main()
+        {
+            GameLevel gl = new GameLevel(15, Difficulty.Hard);
+
+            gl.SetFoeInRoom(2, new Foe("Darth Vader"));
+            gl.SetFoeInRoom(5, new Foe("Borg Queen"));
+            gl.SetFoeInRoom(11, new Foe("Thanos"));
+            gl.SetFoeInRoom(12, new Foe("Xenomorph"));
+
+            Console.WriteLine($"Difficulty: {gl.GetDifficulty()}");
+
+            Console.WriteLine($"Number of rooms: {gl.GetNumRooms()}");
+
+            Console.WriteLine($"Number of foes: {gl.GetNumFoes()}");
+
+            gl.PrintFoes();
+
+            // Este programa mostra o seguinte no ecrã:
+            //
+            // Difficulty: Hard
+            // Number of rooms: 15
+            // Number of foes: 4
+            // Room 2: Darth Vader
+            // Room 5: Borg Queen
+            // Room 11: Thanos
+            // Room 12: Xenomorph
+        }
     }
 }
--- GameSixFriday/Program.cs
+++ GameSixFriday/Program.cs
@@ -2,11 +2,34 @@
 
 namespace GameSixFriday
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            GameLevel gl = new GameLevel(15, Difficulty.Hard);
+
+            gl.SetFoeInRoom(2, new Foe("Darth Vader"));
+            gl.SetFoeInRoom(5, new Foe("Borg Queen"));
+            gl.SetFoeInRoom(11, new Foe("Thanos"));
+            gl.SetFoeInRoom(12, new Foe("Xenomorph"));
+
+            Console.WriteLine($"Difficulty: {gl.GetDifficulty()}");
+
+            Console.WriteLine($"Number of rooms: {gl.GetNumRooms()}");
+
+            Console.WriteLine($"Number of foes: {gl.GetNumFoes()}");
+
+            gl.PrintFoes();
+
+            // Este programa mostra o seguinte no ecrã:
+            //
+            // Difficulty: Hard
+            // Number of rooms: 15
+            // Number of foes: 4
+            // Room 2: Darth Vader
+            // Room 5: Borg Queen
+            // Room 11: Thanos
+            // Room 12: Xenomorph
         }
     }
 }
